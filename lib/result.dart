18bfcf980554545844a6c2ccbf17e809820869e2
muniss950 <munish3971@gmail.com>
2022-11-11 23:04:20 +0530

    updated

--- lib/result.dart
+++ lib/result.dart
@@ -15,15 +15,42 @@ class result extends StatelessWidget{
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  FutureBuilder(
+                  Container(
+                    color: Colors.blue,
+              child:
+              Column(children:[
+
+                FutureBuilder(
                       future:apicall(city),
                       builder: (context,snapshot) {
 
                         if(snapshot.hasData){
                           return Column(
                               children:[
-                            Text(snapshot.data['description'].toString()),
-                            Text("${snapshot.data['temp'].toString()} K")
+                            Text("City name: ${city}",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),),
+                            Text('Description: ${snapshot.data['description'].toString()}',
+                            style: TextStyle(
+                              color:Colors.white
+                            ),),
+                            Text("Temperature: ${snapshot.data['temp'].toString()}C",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),),
+                            Text("Humidity: ${snapshot.data['humidity'].toString()}",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),) ,
+                            Text("Wind Speed: ${snapshot.data['speed'].toString()}",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),),
+                            Text("Visibility: ${snapshot.data['visibility'].toString()}",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),)
                               ]
                           );
                         }
@@ -31,7 +58,9 @@ class result extends StatelessWidget{
                           return CircularProgressIndicator();
                         }
                       }),
-
+            ]
+            )
+            )
                 ],
               ),
             )
@@ -41,15 +70,17 @@ class result extends StatelessWidget{
 }
 Future  apicall(String city) async{
   final url=Uri.parse(
-      "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=12bd29b3a15de53b773e62b28a9d8ea2");
+      "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=12bd29b3a15de53b773e62b28a9d8ea2&units=metric");
   final response=await http.get(url);
   print(response.body);
   final json =jsonDecode(response.body);
   print(json['weather'][0]['description']);
   final output={
     'description': json['weather'][0]['description'],
-    'temp': json['main']['temp']
-
-  };
+    'temp': json['main']['temp'],
+    'humidity': json['main']['humidity'],
+    'pressure': json['main']['pressure'],
+    'speed': json['wind']['speed'],
+    'visibiliy': json['visibility']};
   return output;
 }
